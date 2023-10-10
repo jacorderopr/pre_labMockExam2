b93258ffbeed2024fa68a2c2fa5df731a52a4467
Jose Alejandro <jacorderopr@gmail.com>
2023-10-10 00:33:21 -0400

    contains example code shown in video along with the solution from the video

---
+++ playerSolution.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+enum TEAM {REAL_MADRID, ATELTI};
+class Player{
+
+    private:
+        int age;
+        string sport;
+        TEAM team;
+        string name; // ranges from 1 - 10
+
+    public:
+
+        Player(string name, int age, string sport, TEAM team)
+        {
+            this->name = name; // ranges from 1 - 10
+            this->age = age;
+            this->sport = sport;
+            this->team = team;
+        }
+        Player();
+
+        string getName(){return name;}
+        string getSport(){return sport;}
+        TEAM getTeam(){return team;}
+        int getAge(){return age; }
+
+
+        /*
+        Exercise 1:
+
+        remove the last Player that has a team of REAL_MADRID
+        */
+       static void removeLastPlayerFromRealMadrid(vector<Player> &vec){ // second to last player
+    // removing LAST player from Real Madrid
+      for(int i = vec.size() - 1; i >= 0; i--){
+                Player currentPlayer = vec[i];
+
+                if(currentPlayer.getTeam() == REAL_MADRID){
+                    vec.erase(vec.begin() + i);
+                    return; //break
+                }
+
+            }
+    // removing second to last player from Real Madrid (penúltimo):
+
+
+    //    bool foundFirst = false;
+    //         for(int i = vec.size() - 1; i >= 0; i--){
+    //             Player currentPlayer = vec[i];
+    //             if(foundFirst && currentPlayer.getTeam() == REAL_MADRID){
+    //                 vec.erase(vec.begin() + i);
+    //                 return; //break
+    //             }
+
+    //             if(currentPlayer.getTeam() == REAL_MADRID){
+    //                 foundFirst = true;
+    //             }
+
+    //         }
+       }
+
+    /*
+    Exercise 2:
+    
+    Combine vectors of players into one vector. alternate between the first parameter vector and the second parameter vector BUT go through the second parameter vector backwards
+
+    ASSUME vectors always have the same length!
+
+    Example: 
+    v1 = <apu, jil, leo>
+    v2 = <bob, brad, jude>
+
+    result = <apu, jude, jil, brad, leo, anthony>
+    */
+       static vector<Player> combine(vector<Player> v1, vector<Player> v2){
+        vector<Player> result;
+
+        for(int i = 0; i < v1.size(); i++){
+            Player playerVector1 = v1[i];
+            int secondVectorIndex = v1.size() - 1 - i;
+            Player playerVector2 = v2[secondVectorIndex];
+
+            result.push_back(playerVector1);
+            result.push_back(playerVector2);
+        }
+       return result;
+       }
+    
+};
+
+
+void showVector(vector<Player> v){
+    cout << "{ ";
+    for(auto myIterator = v.begin(); myIterator != v.end(); myIterator++){
+        // cout << "Speed: " << myIterator->getSpeed() << " Age: " << myIterator->getAge() << " sport: " << myIterator->getSport() << " Team: " << myIterator->getTeam();
+        cout << myIterator->getName() ;
+        if(myIterator != v.end() - 1){
+            cout << ", " ;
+        }
+    }
+    cout << "}" << endl;
+}
+
+int main(){
+    // Player(int speed, int age, string sport, string team)
+    Player atleti1 = Player("Grizi", 21, "soccer", ATELTI);
+    Player atleti2 = Player("Depay", 25, "soccer", ATELTI);
+    
+    Player realMadrid1 = Player("Jude", 27, "soccer", REAL_MADRID);
+    Player realMadrid2 = Player("Modric", 34, "soccer", REAL_MADRID);
+    
+
+
+    vector<Player> playerVector = {atleti1,realMadrid1, realMadrid2, atleti2};
+
+    cout << "Before removing last Real Madrid Player: " << endl << endl;
+    showVector(playerVector);
+
+    cout << "After removing last Real Madrid Player: " << endl << endl;
+    Player::removeLastPlayerFromRealMadrid(playerVector);
+   showVector(playerVector);
+
+    vector<Player> vec1 = {atleti1, atleti2}; // <"Grizi", "Depay">
+    vector<Player> vec2 = {realMadrid1, realMadrid2}; // < "Jude", "Modric">
+    cout << endl << endl<< "Exercise 2 After combining: " << endl;
+    vector<Player> result = Player::combine(vec1, vec2); // result should be: <"Grizi", "Modric", "Depay", "Jude"
+    showVector(result);
+}
+
